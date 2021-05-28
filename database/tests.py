e2a4b8d3d3b2af20b88ca4346b85ea4388e8f34e
phuoclm <leminhphuoc04@gmail.com>
2021-05-29 01:52:06 +0700

    test case

--- database/tests.py
+++ database/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
 class URLTests(TestCase):
     def test_home(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
--- test_case.py
+++ test_case.py
@@ -2,12 +2,13 @@ import pytest
 from django.urls import reverse
 from database.models import Model_predict
 
+
 def func(x):
-    return x+5
+    return x + 5
+
+
 def test():
     assert func(4) == 9
-
-
 
 
 from django.contrib.auth.models import User
@@ -15,18 +16,20 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_user_create():
-  User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-  assert User.objects.count() == 1
+    User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    assert User.objects.count() == 1
+
 
 import pytest
 
 from django.urls import reverse
 
+
 @pytest.mark.django_db
 def test_view(client):
-   url = reverse('homepage')
-   response = client.get(url)
-   assert response.status_code == 200
+    url = reverse('homepage')
+    response = client.get(url)
+    assert response.status_code == 200
 # @pytest.mark.django_db
 # def test_unauthorized(client):
 #    url = reverse('predictImage')
