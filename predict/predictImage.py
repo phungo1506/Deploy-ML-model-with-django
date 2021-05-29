48fa07f69f82ac883e982b4cb22f43f65742b4ec
ngochriscs12 <18521243@gm.uit.edu.vn>
2021-05-29 21:30:00 +0700

    update code and docs

--- predict/predictImage.py
+++ predict/predictImage.py
@@ -1,36 +1,20 @@
-from PIL import Image
 import cv2
 import numpy as np
 from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render
-from . import get_output_layers
 
 class predict_Image:
-    
+    """
+        perform some functions like:
+            1. get the link of the image from the wed
+            2. draw the bounding box
+            3. perform object prediction from models
+            4. draw bounding box after prediction   
+        Arg:
+    """
+
     def __init__(self):
         super().__init__()
 
-    def draw_prediction(self,classes,COLORS, img,class_id, confidence, x, y, x_plus_w, y_plus_h):
-        """Args:
-                classes: name of object
-                COLORS: random color in (0,255)
-                img: image input
-                confidence: accuracy predict
-                x: x center
-                y: y center
-                x_plus_w: x + width
-                y_plus_h: y + height
-            Return
-                the image has been drawn bounding box after prediction
-        """
-        label = str(classes[class_id])
-
-        color = COLORS[class_id]
-
-        cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
-
-        cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    
     def get_url_img(self,request):
         """
             Arg:
@@ -38,18 +22,36 @@ class predict_Image:
             Return:
                 link of input image use preidct
         """
+
         fileObj=request.FILES['filePath']
         fs=FileSystemStorage()
         filePathName=fs.save(fileObj.name,fileObj)
         filePathName=fs.url(filePathName)
-        testimage='.'+filePathName
+        testimage='.' + filePathName
         return testimage
-    def predict_object(self,outs,Width,Height):
+
+    def draw_bbox(self,classes,COLORS, img,class_id, bbox):
+        """Args:
+                classes: name of object
+                COLORS: random color in (0,255)
+                img: image input
+                bbox: a list of x_center, y_center, Width, Height
+            Return
+                the image has been drawn bounding box after prediction
+        """
+        label = str(classes[class_id])
+
+        color = COLORS[class_id]
+
+        cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), color, 2)
+
+        cv2.putText(img, label, (bbox[0] - 10, bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2) 
+
+    def predict_object(self,outs,image):
         """
             Arg:
                 outs: Numpy ndarray as output which you can use it to plot box on the given input image.
-                Width : Width of image
-                Height : Height of image
+                image: input image
             Return:
                 boxes: A list containing the bboxes of the predicted object
                 confidences: A list containing the object's accuracy
@@ -58,6 +60,8 @@ class predict_Image:
         class_ids = []
         confidences = []
         boxes = []
+        Width = image.shape[1]
+        Height = image.shape[0]
         for out in outs:
             for detection in out:
                 scores = detection[5:]
@@ -74,7 +78,8 @@ class predict_Image:
                     confidences.append(float(confidence))
                     boxes.append([x, y, w, h])
         return boxes,confidences,class_ids
-    def draw_bbox_prediction(self,indices,COLORS,image,classes,class_ids,boxes,confidences):
+
+    def draw_bbox_prediction(self,indices,COLORS,image,classes,class_ids,boxes):
         """
             Arg:
                 indices: Numpy ndarray as output which you can use it to plot box on the given input image.
@@ -83,16 +88,16 @@ class predict_Image:
                 classes: list class of object
                 class_ids: A list containing the ids to which the object belongs
                 boxes: A list containing the bboxes of the predicted object
-                confidences: A list containing the object's accuracy
+
             Return:
                 image: the image has been drawn bounding box after prediction
         """
         for i in indices:
             i = i[0]
             box = boxes[i]
-            x = box[0]
-            y = box[1]
-            w = box[2]
-            h = box[3]
-            self.draw_prediction(classes,COLORS,image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+            box[0] = round(box[0])
+            box[1] = round(box[1])
+            box[2] = round(box[2])
+            box[3] = round(box[3])
+            self.draw_bbox(classes,COLORS,image, class_ids[i], box)
         return image
