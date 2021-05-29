e7bc45c125c36fd27b09fd1a07e761614a4fdc4d
ngochriscs12 <18521243@gm.uit.edu.vn>
2021-05-29 11:14:02 +0700

    update code and docs

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 import cv2
-import time
 import numpy as np
 from predict import predictImage,get_output_layers
 
@@ -29,67 +28,29 @@ def predict(request):
                 )
     """
     classes = None
-
     with open('./models/yolov4.txt', 'r') as f:
         classes = [line.strip() for line in f.readlines()]
-
     COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
     session = predictImage.predict_Image()
     testimage = session.get_url_img(request)
     img = testimage
-
     image = cv2.imread(testimage)
-
     Width = image.shape[1]
     Height = image.shape[0]
     scale = 0.00392
-
-
     net = cv2.dnn.readNet('./models/yolov4.weights', './models/yolov4.cfg')
-
     blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
-
     net.setInput(blob)
-    
     outs = net.forward(get_output_layers.get_output_layers(net))
-
-    class_ids = []
-    confidences = []
-    boxes = []
     conf_threshold = 0.5
     nms_threshold = 0.4
-
-    # Thực hiện xác định bằng HOG và SVM
-    start = time.time()
-
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
-
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        session.draw_prediction(classes,COLORS,image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+    boxes = session.predict_object(outs,Width,Height)[0]
+    confidences = session.predict_object(outs,Width,Height)[1]
+    class_ids = session.predict_object(outs,Width,Height)[2]
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold) #non maximum suppression
+    image = session.draw_bbox_prediction(indices,COLORS,image,classes,class_ids,boxes,confidences)
     name_img = testimage[:-4] + '_predict.jpg'
+    
     cv2.imwrite(name_img, image)
     img_predict = name_img
     model = Model_predict(img=img, img_predict = img_predict)
--- predict/predictImage.py
+++ predict/predictImage.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import cv2
-import time
 import numpy as np
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
@@ -45,3 +44,55 @@ class predict_Image:
         filePathName=fs.url(filePathName)
         testimage='.'+filePathName
         return testimage
+    def predict_object(self,outs,Width,Height):
+        """
+            Arg:
+                outs: Numpy ndarray as output which you can use it to plot box on the given input image.
+                Width : Width of image
+                Height : Height of image
+            Return:
+                boxes: A list containing the bboxes of the predicted object
+                confidences: A list containing the object's accuracy
+                class_ids: A list containing the ids to which the object belongs
+        """
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * Width)
+                    center_y = int(detection[1] * Height)
+                    w = int(detection[2] * Width)
+                    h = int(detection[3] * Height)
+                    x = center_x - w / 2
+                    y = center_y - h / 2
+                    class_ids.append(class_id)
+                    confidences.append(float(confidence))
+                    boxes.append([x, y, w, h])
+        return boxes,confidences,class_ids
+    def draw_bbox_prediction(self,indices,COLORS,image,classes,class_ids,boxes,confidences):
+        """
+            Arg:
+                indices: Numpy ndarray as output which you can use it to plot box on the given input image.
+                COLORS : A nparray of color codes
+                image : input image
+                classes: list class of object
+                class_ids: A list containing the ids to which the object belongs
+                boxes: A list containing the bboxes of the predicted object
+                confidences: A list containing the object's accuracy
+            Return:
+                image: the image has been drawn bounding box after prediction
+        """
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            self.draw_prediction(classes,COLORS,image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+        return image
