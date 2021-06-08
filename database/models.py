245f2670a3370e3d8a133a70b0b881bf2824a2ee
phuoclm <leminhphuoc04@gmail.com>
2021-06-08 22:45:57 +0700

    update cmt

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from django.db import  models
 
 class Model_predict(models.Model):
-    img = models.CharField(max_length=100000000000)
-    img_predict = models.CharField(max_length=100000000000)
+    img = models.CharField(max_length=1000)
+    img_predict = models.CharField(max_length=1000)
