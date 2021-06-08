41c2252c680dacbdfcfb821358a30df02f6c044e
Le Minh Phuoc <46823545+phuoclm404@users.noreply.github.com>
2021-06-08 22:51:55 +0700

    update cmt code

--- test/test_pass-circleci.py
+++ test/test_pass-circleci.py
@@ -2,6 +2,7 @@ import pytest
 """
     This test case is created for the purpose of performing the demo tool CircleCI.
     No effect in testing the project.
+    Github is missing an important file. It is file model (yolov4.weights) in folder models, our team can't upload it to github because of the size limit of github
 """
 def func(x):
     return x + 5
